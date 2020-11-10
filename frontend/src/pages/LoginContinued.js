caf01b3ef2bfc36c6011a5ba71927b23bad630d8
vitor-risso <vitor.risso13@gmail.com>
2020-11-10 19:14:24 -0300

    segunda pagina de login criada, proximo passo questoes. obs: não consegui arrumar os termos

--- frontend/src/pages/LoginContinued.js
+++ frontend/src/pages/LoginContinued.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+import '../styles/pages/loginContinued.css'
 
 function LoginContinued (){
     return(
@@ -9,6 +12,25 @@ function LoginContinued (){
                     <p>Frase sobre a nossa ideia</p>
                 </main>
             </div>
+
+            <div className='content'>
+                <h2>Quase lá</h2>
+                <p>Nos diga como podemos chamar você e crie uma senha segura para acessar a conta..</p>
+            </div>
+
+            <div className='name'>
+                <p>Nome:</p>
+                <input type='text'  />
+            </div>
+
+            <div className='password'>
+                <p>Senha:</p>
+                <input type='text'  />
+            </div>
+            
+                <p>Li e concordo com os termos de uso</p>
+                
+            <Link to='/' className='create-account'>Criar conta</Link>
         </div>
     )
 }
