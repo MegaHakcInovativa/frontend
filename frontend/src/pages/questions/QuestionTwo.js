2b481001b13752993946e4f7410f671ec2d8d630
vitor-risso <vitor.risso13@gmail.com>
2020-11-10 21:37:33 -0300

    primeira questao criada segunda iniciada

---
+++ frontend/src/pages/questions/QuestionTwo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TopBar from '../components/topBar/TopBar';
+
+function QuestionTwo(){
+    return(
+        <div className='question'>
+            
+        </div>
+    )
+}
